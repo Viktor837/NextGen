4ff5ea87431652e6fa769be17aec011cced8a69c
Viktor837 <viktor.volkov.2000@bk.ru>
2020-03-13 10:26:52 +0700

    Izmenil SalesLineItem

--- WindowsProject1/WindowsProject1/bissness logika/SalesLineItem.h
+++ WindowsProject1/WindowsProject1/bissness logika/SalesLineItem.h
@@ -1,12 +1,18 @@
 #pragma once
-#include "ProductDescription.h"
-#include "Money.h"
+
 public class SalesLineItem
 {
-private: 
+private:
 	int quantity;
 	ProductDescription description;
 public:
-	SalesLineItem(ProductDescription desc, int quantity);
-	int getSubtotal();
+	SalesLineItem(ProductDescription desc, int quantity)
+	{
+		this.description = desc;
+		this.quantity = quantity;
+	}
+	Money getSubtotal()
+	{
+		return description.getPrice().times(quantity);
+	}
 }
