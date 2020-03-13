7d8fd3da08f68eb815c3ba999ff2be5e65324226
Viktor837 <viktor.volkov.2000@bk.ru>
2020-03-13 10:19:10 +0700

    Dobavil class Sale

--- WindowsProject1/WindowsProject1/bissness logika/Sale.h
+++ WindowsProject1/WindowsProject1/bissness logika/Sale.h
@@ -3,3 +3,36 @@
 public class Sale
 {
 private:
+	List<SalesLineItem> lineitem = new ArrayList() < SalesLineItem > ;
+	Date date = new Date();
+	boolen isComplete = false;
+	Payment payment;
+public:
+	Money getBalance()
+	{
+		payment.getAmount().minus(getTotal());
+	}
+	void becomeComplete() {
+		isComplete = true;
+	}
+	boolean isComplete() { return isComplete; }
+	void makeLineItem(ProductDescription desc, int quantity)
+	{
+		lineItems.add(new SaleLineItem(desc, quantity));
+	}
+	Money getTotal()
+	{
+		Money total = new Money();
+		Money subtotal = null;
+		for (SalesLineItem lineItem : lineItems)
+		{
+			subtotal = lineItem.getSubtotal();
+			total.add(subtotal);
+		}
+		return total;
+	}
+	void makePayment(Money cashTendered)
+	{
+		payment = new Payment(cashTendered);
+	}
+}
